76d6365c0efd1d1c4d384cfaf4950b8dca2ad010
fadyat <fadyat@icloud.com>
2022-06-16 22:39:57 +0300

    commented wc test

--- BashWrapperTests/Program.cs
+++ BashWrapperTests/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using BashWrapper;
 using BashWrapper.Outputs;
 using NUnit.Framework;
@@ -153,13 +154,7 @@ public class Tests
         var actual = _stringWriter.ToString()[..expected.Length];
         Assert.AreEqual(expected, actual);
     }
-
-    [Test]
-    public void WcTest()
-    {
-        // ...
-    }
-
+    
     [Test]
     public void AndConnectorTestTrue()
     {
